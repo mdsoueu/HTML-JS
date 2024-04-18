fc0fba81ec7e509aa3c64c284897e455762f3270
Maria Oliveira <128597411+mdsoueu@users.noreply.github.com>
2024-04-18 19:23:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,15 +18,19 @@ function cadastro() {
 
     // nome - backgroud verde, color vermelho
     tabelaDeCadastro.innerHTML += `<p style= "display: inline-block; background-color: green; color: red; padding-right: 2px;"><strong>Nome: </strong>${nome.value}</p>`;
+     // tabelaDeCadastro.innerHTML += `<p style="display: inline-block; padding-right: 2px;"><span style="background-color: green; color: red;">Nome: ${nome.value}</span></p>`;
     
     // data - backgroud preto, color branco
     tabelaDeCadastro.innerHTML += `<p style= "display: inline-block; background-color: black; color: white; padding-right: 2px;"><strong>Data: </strong>${dataNascimento.value}</p>`;
+    // tabelaDeCadastro.innerHTML += `<p style= "display: inline-block; padding-right: 2px;"><span style=" background-color: black; color: white;">Data: ${dataNascimento.value}</span></p>`;
     
     // email - backgroud amarelo, color roxo
     tabelaDeCadastro.innerHTML += `<p style= "display: inline-block; background-color: yellow; color: purple; padding-right: 2px;"><strong>Email: </strong>${email.value}</p>`;
+    // tabelaDeCadastro.innerHTML += `<p style= "display: inline-block; padding-right: 2px;"><span style=" background-color: yellow; color: purple;">Email: ${email.value}</span></p>`;
     
     // senha - backgroud cinza, color azul
     tabelaDeCadastro.innerHTML += `<p style= "display: inline-block; background-color: gray; color: blue; padding-right: 2px;"><strong>Senha: </strong>${senha.value}</p>`;
-
+    // tabelaDeCadastro.innerHTML += `<p style= "display: inline-block; padding-right: 2px;"><span style=" background-color: gray; color: blue;">Senha: ${senha.value}</span></p><br>`;
+    
     console.log("sucesso");
 }
